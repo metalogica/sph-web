4aae8c3568b946cfba7d122e37b96f5e0d57c9a7
RJ <richard.james.jarram@gmail.com>
2025-01-11 19:33:30 -0500

    rmeove banner

--- js/ptc-banner.js
+++
@@ -1,6 +0,0 @@
-var stickBannerContainer = document.querySelector('.sticky-banner');
-var closeButton = document.getElementById('sticky-banner-close-button');
-
-closeButton.addEventListener('click', function(event) {
-  stickBannerContainer.classList.add('sticky-banner-fade-out');
-});
